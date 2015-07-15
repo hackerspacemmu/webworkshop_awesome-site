a53ebc4ca56148190e9e8f0c5267b697afbacf2e
Yong Lian Hii <Hii@Yongs-MacBook-Pro.local>
2015-07-16 02:45:16 +0800

    Corrected indentation separated fiels into partials and added basic search

--- _about.php
+++ _about.php
@@ -1,41 +1,37 @@
-		<div id="main">
-			<div id="left-container">
-				<h3>This text is inside the<br>#left-container</h3>
-				<img src="avatar.png">
-			</div>
-			<div id="right-container">
-				<h3>This text is inside the #right-container</h3>
-				<h2 class="page-title">About</h2>
-				<p class="text">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-					sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-			</div>
-		</div>
+<div id="main">
+  <div id="left-container">
+    <h3>This text is inside the<br>#left-container</h3>
+    <img src="avatar.png">
+  </div>
+  <div id="right-container">
+    <h3>This text is inside the #right-container</h3>
+    <h2 class="page-title">About</h2>
+    <p class="text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </div>
+</div>
 
-		<div id="built-with">
-			<hr>
-			<div class="item">
-				<img src="html5.png">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.</p>
-			</div>
-			<div class="item">
-				<img src="css3.png">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.</p>
-			</div>
-			<div class="item">
-				<img src="php.png">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.</p>
-			</div>
-		</div>
-
-	</div>
-</body>
-</html>
+<div id="built-with">
+  <hr>
+  <div class="item">
+    <img src="html5.png">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+  <div class="item">
+    <img src="css3.png">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+  <div class="item">
+    <img src="php.png">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+</div>
--- home.php
+++ home.php
@@ -7,66 +7,34 @@
 ?>
 <!doctype html>
 <html>
-<head>
-  <title>Awesome Site - Home</title>
-  <link rel="stylesheet" type="text/css" href="awesome-site.css">
-</head>
-<body>
-  <div class="container">
-    <div id="header">
-      <h1>Joey's Awesome Site</h1>
-    </div>
-    <form method="get" action="home.php">
-    Don't Like the Color? Enter a Color to Change it!
-    <input type="text" name="color">
-    </form>
-    <div id="navigation">
-      <a href="home.php?section=home" class="link">Home</a>
-      <a href="home.php?section=about" class="link">About</a>
-    </div>
-    <?php if($section == "home") { ?>
-    <div id="main">
-      <div id="left-container">
-        <h3>Profile</h3>
-        <img src= <?= $picture[rand(0,2)] ?> width="250px" height="250px">
+  <head>
+    <title>Awesome Site - Home</title>
+    <link rel="stylesheet" type="text/css" href="awesome-site.css">
+  </head>
+  <body>
+    <div class="container">
+      <div id="header">
+        <h1>Joey's Awesome Site</h1>
       </div>
-      <div id="right-container">
-
-        <h3>The Awesomest site in the history of the internet</h3>
-        <div class="post-container">
-          <h2 class="page-title"><?= $title ?></h2>
-          <p class="text"><?= $content ?></p>
-        </div>
-        <div class="post-container">
-          <h2 class="page-title">Home</h2>
-          <p class="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+      <form method="get" action="home.php">
+      Don't Like the Color? Enter a Color to Change it!
+      <input type="text" name="color">
+      </form>
+      <div id="navigation">
+        <a href="home.php?section=home" class="link">Home</a>
+        <a href="home.php?section=about" class="link">About</a>
       </div>
+      <?php if($section == "home" || !isset($section)) {
+        include "_home.php";
+      }else if($section == "about"){
+        include "_about.php";
+      }
+      ?>
     </div>
-    <form method="POST" action="home.php" class="form-control">
-      <h3>Submit a status update!</h3>
-      <div class="form-label">  Title:  </div>
-<input type="text" name="title"><br><br>
-      <div class="form-label"> Content: </div> <textarea name="content"></textarea> <br>
-
-      <input type="submit">
-    </form>
-  <?php } else {
-    include "_about.php";
-    } ?>
-  </div>
-
-</body>
+  </body>
 </html>
 <style>
-#header {
-  background: <?= $color ?>;
-}
+  #header {
+    background: <?= $color ?>;
+  }
 </style>
